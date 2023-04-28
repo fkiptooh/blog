50a25dc203b48800b3b96fc28150c55e2d58f9a3
fkiptooh <fkiptooh@gmail.com>
2023-04-28 21:36:46 +0300

    Cloudinary cdn

--- app/components/inputs/ImageUpload.tsx
+++ app/components/inputs/ImageUpload.tsx
@@ -35,7 +35,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
       {({ open }) => {
         return (
           <div
-            onClick={() => open && open()}
+            onClick={() => open?.()}
             className="
               relative
               cursor-pointer
